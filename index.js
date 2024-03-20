6306c2197edbf1c7337969fdf9f84bc2a3102194
Mark <mrkcarlos576@gmail.com>
2024-03-20 15:30:35 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+let companyNamee = 'Scuber';
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhoodd = 'Chelsea';
+
+const mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeoo = 'Susan Smith'
+
+const companyCeo = 'Susan Smith'
+
+
+
+
